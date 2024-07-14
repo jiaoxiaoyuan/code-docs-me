df1f77c4f9e29b95e3445cfd73881a3cce3aa74b
JIAOXIAOYUAN <329492979@qq.com>
2024-07-14 17:30:10 +0800

    ✨ feat: 优化代码

--- .vitepress/theme/components/layout/APlayer.vue
+++ .vitepress/theme/components/layout/APlayer.vue
@@ -7,15 +7,7 @@ import { getMusic } from "../../../api/";
 const lrcShow = ref<boolean>(false);
 const autoplay = ref<boolean>(false);
 const aplayer = ref<any>(null);
-const audio = ref<any[]>([
-    {
-        "title": "Legends Never Die",
-        "author": "Against the Current",
-        "url": "https://api.i-meto.com/meting/api?server=netease&type=url&id=506196018&auth=c5c21102c12896557ecf3fd43a415c050fe757a6",
-        "pic": "https://api.i-meto.com/meting/api?server=netease&type=pic&id=109951163918904060&auth=ab3ada1788834c2c30fc14be2320219dbeaebc12",
-        "lrc": "https://api.i-meto.com/meting/api?server=netease&type=lrc&id=506196018&auth=9038409d31ec7f40ea7888b77f0839051a2859a5"
-    },
-]);
+const audio = ref<any[]>([]);
 
 const APlayer = defineClientComponent(() => {
     return import('../../../utils/vue-aplayer/vue-aplayer.js')
