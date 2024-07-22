49a30ca2d10bd2aff18c38157952edd599dff4a1
JIAOXIAOYUAN <329492979@qq.com>
2024-07-22 23:04:21 +0800

    ✨ feat: bug

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -15,6 +15,7 @@ import "element-plus/dist/index.css";
 import giscusTalk from "vitepress-plugin-comment-with-giscus";
 
 
+
 // 只需添加以下一行代码，引入时间线样式
 import "vitepress-markdown-timeline/dist/theme/index.css";
 
--- .vitepress/theme/useComponents.ts
+++ .vitepress/theme/useComponents.ts
@@ -7,6 +7,7 @@ import BasePage from "./components/Base/index.vue";
 
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
+
 export function useComponents({app}: EnhanceAppContext) {
     app.component("MNavLinks", MNavLinks);
     app.component("HomeIndex", HomeIndex);
